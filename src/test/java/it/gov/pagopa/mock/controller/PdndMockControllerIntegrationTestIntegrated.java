105810a455fc3770819fe8adc57139d1472dc5eb
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2023-09-13 11:04:22 +0300

    [IDP-1731] fixed junit

--- src/test/java/it/gov/pagopa/mock/controller/PdndMockControllerIntegrationTestIntegrated.java
+++ src/test/java/it/gov/pagopa/mock/controller/PdndMockControllerIntegrationTestIntegrated.java
@@ -11,7 +11,6 @@ import org.springframework.test.context.TestPropertySource;
         "classpath:/mongodbEmbeddedDisabled.properties",
         "classpath:/secrets/mongodbConnectionString.properties"
 })
-@Disabled
 class PdndMockControllerIntegrationTestIntegrated extends PdndMockControllerIntegrationTest{
 
 }

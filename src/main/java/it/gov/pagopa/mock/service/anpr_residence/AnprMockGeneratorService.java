a41a10a260cb03a3f615f4e770bfb8e781f1d7ca
Benedetta-fabbri <124246048+Benedetta-fabbri@users.noreply.github.com>
2023-09-26 15:23:34 +0200

    Revert "feat: [IDP-1859] mock anpr residence api"

--- src/main/java/it/gov/pagopa/mock/service/anpr_residence/AnprMockGeneratorService.java
+++
@@ -1,8 +0,0 @@
-package it.gov.pagopa.mock.service.anpr_residence;
-
-import it.gov.pagopa.mock.dto.anpr.AnprRequestDTO;
-import it.gov.pagopa.mock.dto.anpr.AnprResponseDTO;
-
-public interface AnprMockGeneratorService {
-    AnprResponseDTO getAnprResidence(AnprRequestDTO anprRequestDTO);
-}
--- src/test/java/it/gov/pagopa/mock/utils/UtilitiesTest.java
+++ src/test/java/it/gov/pagopa/mock/utils/UtilitiesTest.java
@@ -1,13 +1,9 @@
 package it.gov.pagopa.mock.utils;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-
-import java.time.LocalDate;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -26,24 +22,5 @@ class UtilitiesTest {
         Utilities.performanceLog(System.currentTimeMillis(), "");
 
         assertEquals(result, PageRequest.of(0, 15, Sort.by("updateDate")));
-    }
-
-    @Test
-    void testBirthDateFromFiscalCodeAndAge() {
-        Map<String, LocalDate> mapFiscalCodesBirthDates = Map.of(
-                "PTRGNL73S51X000Q", LocalDate.of(1973, 11, 11),
-                "BLBGRC57A05X000D", LocalDate.of(1957, 1, 5),
-                "DSUFLV00R19X000P", LocalDate.of(2000, 10, 19),
-                "BCCVCR01E65X000T", LocalDate.of(2001, 5, 25)
-        );
-
-        for(String fc : mapFiscalCodesBirthDates.keySet()) {
-            LocalDate result = Utilities.calculateBirthDateFromFiscalCode(fc);
-
-            Assertions.assertEquals(mapFiscalCodesBirthDates.get(fc), result);
-        }
-
-        int age = Utilities.getAge(LocalDate.now().minusYears(20).plusDays(1));
-        Assertions.assertEquals(19, age);
     }
 }
